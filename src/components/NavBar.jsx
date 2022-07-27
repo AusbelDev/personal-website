a72e4c31ed47c9db5ee568435f2083238eaa7dfb
Ausbel <mr.rochahernandez@gmail.com>
2022-07-27 00:21:13 -0500

    Changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,9 +12,11 @@ import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import CodeIcon from "@mui/icons-material/Code";
+import { NearMeDisabledTwoTone } from "@mui/icons-material";
 
 const pages = ["About Me", "Portfolio", "Contact Me"];
 const settings = ["Profile", "Account", "Dashboard", "Logout"];
+const name = "<Λ>";
 
 const ResponsiveAppBar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
@@ -39,7 +41,7 @@ const ResponsiveAppBar = () => {
     <AppBar position="static">
       <Container maxWidth="false">
         <Toolbar disableGutters>
-          <CodeIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          {/* <CodeIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} /> */}
           <Typography
             variant="h6"
             noWrap
@@ -48,14 +50,14 @@ const ResponsiveAppBar = () => {
             sx={{
               mr: 2,
               display: { xs: "none", md: "flex" },
-              fontFamily: "Ubuntu, monospace",
+              fontFamily: "Ubuntu Mono, monospace",
               fontWeight: 700,
               letterSpacing: ".3rem",
               color: "inherit",
               textDecoration: "none",
             }}
           >
-            Λusbel
+            {name}
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
@@ -94,7 +96,7 @@ const ResponsiveAppBar = () => {
               ))}
             </Menu>
           </Box>
-          <CodeIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          {/* <CodeIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} /> */}
           <Typography
             variant="h5"
             noWrap
@@ -104,14 +106,14 @@ const ResponsiveAppBar = () => {
               mr: 2,
               display: { xs: "flex", md: "none" },
               flexGrow: 1,
-              fontFamily: "Ubuntu, monospace",
+              fontFamily: "Ubuntu Mono, monospace",
               fontWeight: 700,
               letterSpacing: ".3rem",
               color: "inherit",
               textDecoration: "none",
             }}
           >
-            Λusbel
+            {name}
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
